84bc18590c4d9f4cf6cfc2861f98982306bb2043
ha_ suke <hasuke8arusuke@gmail.com>
2019-02-28 01:22:19 +0900

    work pageホバーエフェクトとモーダル実装

--- html/js/style.js
+++ html/js/style.js
@@ -1,20 +1,43 @@
-//親Swiper
-var mySwiperp = new Swiper('.swiper-parent', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-    }
+// 戻るボタン
+$(function() {
+    var $pageTop = $(".pagetop")
+    $pageTop.hide();
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
+            $pageTop.fadeIn();
+        } else {
+            $pageTop.fadeOut();
+        }
+    });
+    $('a[href^="#"]').click(function() {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href =="" ? 'html' : href);
+        var position = target.offset().top;
+        $("html,body").animate({
+            scrollTop: position
+        }, 500, "swing");
+        return false;
+    });
 });
-//子Swiper
-var mySwiperc = new Swiper('.swiper-child', {
-    nested: true,
-    slidesPerColumnFill: 'row',
-    slidesPerColumn: 2,
-    slidesPerView: 3,
-    slidesPerGroup: 6
+
+// work page hover effect
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+ $('.js-btn-modal').on('click', function(){
+  $('#overlay').fadeIn();
+  var id = $(this).data('id');
+  $('.js-modal[data-id="modal' + id + '"]').fadeIn();
+});
+
+$('.js-close-btn').on('click', function(){
+  $('#overlay').fadeOut();
+  $('.js-modal').fadeOut();
+});
+$('#overlay').on('click', function(){
+  $('#overlay').fadeOut();
+  $('.js-modal').fadeOut();
 });
