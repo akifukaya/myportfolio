f57d78780440d839ad6d483a619a6f8870239f31
zd3f12 <zd3f12@gmail.com>
2019-03-05 15:45:57 +0900

    footer調整　workだけ調整必要

--- html/js/style.js
+++ html/js/style.js
@@ -38,8 +38,3 @@ $('.js-close-btn').on('click', function(){
   $('.overlay').fadeOut();
   $('.js-modal').fadeOut();
 });
-$('.overlay').on('click', function(){
-  $('.overlay').fadeOut();
-  $('.js-modal').fadeOut();
-});
-
